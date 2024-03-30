5b430d89dcbd92c0769b7afc2f64f52c23887286
Charlie-MaxHadibi <51676958+Charlie-MaxHadibi@users.noreply.github.com>
2024-03-30 22:43:52 +0100

    mm

--- index.php
+++ index.php
@@ -8,9 +8,8 @@
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <!--Import materialize.css-->
 <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-<!--Let browser know website is optimized for mobile-->
+<!--Let browser know website is optimized for mobile -->
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<!--import stylce.css -->
 <link type="text/css" rel="stylesheet" href="css/style.css"/>
 </head>
 <body>
@@ -21,7 +20,7 @@
             <div class="row">
 
 
-                <div class="col s1"></div>
+               
 
                 <div class="col s4 formstart">
                     <form  action="hostcopy.php">
@@ -35,7 +34,7 @@
                     
                 </div>
 
-                <div class="col s2"></div>
+                
 
                 <div class="col s4 formstart">
                     <form action="join copy.php" method="POST">
@@ -43,43 +42,72 @@
                             <h2>Rejoins une partie !</h2>
                             <p> Join</p>
                             <input type="number" name="seed" placeholder="Code partie" required="requis">
-                            <input type="submit" value="Rejoindre"">
+                            <input type="submit" value="Rejoindre">
                         </div>
                     </form>
                 </div>
 
-                <div class="col s1"></div>
-
+             
                 <!--
                 <div class="col s4 formstart">
-                    <form action="insert.php" method="POST">
-                        <h2>Ajoute une photo ! </h2>
-                        <p>EN COURS DE DEVELOPPEMENT</p>
-                        <?php
-                        /*
-                            //connexion a la bdd
-                            $servername = "localhost";
-                            $db_username = "root";
-                            $db_password = "1015";
-                            $dbname = "test";
-                            $conn = new mysqli($servername, $db_username, $db_password, $dbname);
+                    <form action="insert.php" method="POST" enctype="multipart/form-data">
+                        <div class="cardd">
+                            <h2>Ajoute une photo ! </h2>
+                            <p>EN COURS DE DEVELOPPEMENT</p>
+                            <?php
+                            /*
+                                //connexion a la bdd
+                                $servername = "localhost";
+                                $db_username = "root";
+                                $db_password = "1015";
+                                $dbname = "test";
+                                $conn = new mysqli($servername, $db_username, $db_password, $dbname);
 
-                            //recuperation des nom existant et les affiche dans une liste
-                            $sql = "SELECT prenom FROM personne GROUP BY prenom";
-                            $result = $conn->query($sql);
-                            $i=1;
-                            echo '<select name="prenom">';
-                            foreach($result as $row){
-                                echo '<option value="'.$row['prenom'].'">'.$row['prenom'].'</option>';
-                            }
-                            echo '</select>';
-                        */
-                        ?>
-                        <input type="file" id="file" name="file">
-                        <input type="submit" value="Ajouter">
+                                //recuperation des nom existant et les met dans un tableau
+                                $sql = "SELECT prenom FROM personne GROUP BY prenom";
+                                $result = $conn->query($sql);
+                                
+                                $i=0;
+                                foreach($result as $row){
+                                    //echo '<option value="'.$row['prenom'].'">'.$row['prenom'].'</option>';
+                                    $prenom[$i] = $row['prenom'];
+                                    $i++;
+                                }
+                                $compteurnom = $i;
+                                // affichage des prenom dans une liste deroulante
+                                echo '<div class="input-field">';
+                                    echo '<select>';
+                                    for ($i=0;$i<$compteurnom;$i++){
+                                        echo '<option value="'.$prenom[$i].'">'.$prenom[$i].'</option>';
+                                        echo $prenom[$i];
+                                    }
+
+                                    echo '</select>';
+                                echo '</div>';
+                                */
+
+                                ?>
+                                
+                                    <select multiple>
+                                        
+                                        <option value="1">Option 1</option>
+                                        <option value="2">Option 2</option>
+                                        <option value="3">Option 3</option>
+                                    </select>
+                                
+                                    <input type="file" id="file" name="file">
+                                    <br>
+                                    <input type="submit" value="Ajouter">
+                                
+                           
+                            
+                        </div>
                     </form>
+                            
                 </div>
-                -->
+                                -->
+       
+                
 
             </div>
         </div>
@@ -88,6 +116,11 @@
 
         <?php include('footer.php'); ?>
     <script src="script.js"></script>
+    <script>
+         $(document).ready(function() {
+    $('select').material_select();
+  });
+    </script>
 </body>
 
 </html>
